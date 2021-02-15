eb594c9d952696c942edb4a8131a222b3ef20efa
Laura <laurabande10@gmail.com>
2021-02-15 19:27:16 +0100

    Realización Práctica

---
+++ src/main/java/domino2/PruebaDomino2.java
@@ -0,0 +1,38 @@
+package domino2;
+
+import java.util.ArrayList;
+
+public class PruebaDomino2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Domino d1 = new Domino();
+		
+		/* Mostramos el número de fichas inicial*/
+		System.out.println("El número de fichas actual es de: " + d1.numeroFichas());
+
+		FichaDomino fd1 = new FichaDomino(5,6);
+		FichaDomino fd2 = new FichaDomino(5,2);
+		d1.eliminarFicha(fd1);
+		d1.eliminarFicha(fd2);
+		/* Mostramos el número de ficchas después de borrar dos*/
+		System.out.println("El número de fichas actual es de: " + d1.numeroFichas());
+		
+		d1.introducirFicha(fd1);
+		/* Mostramos el número de ficchas después de añadir una*/
+		System.out.println("El número de fichas actual es de: " + d1.numeroFichas());
+		
+		System.out.println("¿Contiene el set de fichas elemntos? " + d1.tieneFichas());
+		
+		ArrayList <FichaDomino> listaFichas = d1.fichas();
+		
+		System.out.println("El listado de las fichas es: ");
+		listaFichas.forEach(item -> System.out.println(item.toString()));
+
+		System.out.println("¿Contiene el set de fichas la ficha [5|6]? " + d1.contieneFicha(fd1));
+		System.out.println("¿Contiene el set de fichas la ficha [5|2]? " + d1.contieneFicha(fd2));
+		
+	}
+
+}
