5afa955d8ce396176d8ee83adb894eeaad4a71d0
Laura <laurabande10@gmail.com>
2021-02-16 13:25:41 +0100

    Corrección de número de fichas

--- src/main/java/domino2/Domino.java
+++ src/main/java/domino2/Domino.java
@@ -5,60 +5,58 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class Domino {	
-	
-	Set<FichaDomino> juegoDomino;
+public class Domino {
 
-	public Domino() {
-		super();
-		this.juegoDomino = new HashSet<>();
-		
-		//llenamos el set
-		for(int i = 0; i <= 6; i++) {
-			for(int j = 0; j <=6; j++) {
-				juegoDomino.add(new FichaDomino(i,j));
-			}
-		}
-	}
-	
-	public int numeroFichas() {
-		return juegoDomino.size();
-	}
-	
-	public void introducirFicha(FichaDomino fd) {
-		juegoDomino.add(fd);
-	}
-	
-	public boolean tieneFichas() {
-		return !juegoDomino.isEmpty();
-	}
-	
-	public boolean contieneFicha(FichaDomino fd) {
-		//return juegoDomino.contains(fd);
-		Iterator<FichaDomino> it = juegoDomino.iterator();
-		while(it.hasNext()) {
-			if(it.next().equals(fd)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public ArrayList fichas() {
-		ArrayList<FichaDomino> listaFichas = new ArrayList<>();
-		Iterator<FichaDomino> it = juegoDomino.iterator();
-		while(it.hasNext()) {
-		    listaFichas.add((FichaDomino)it.next());
-		}
-		return listaFichas;
-	}
-	
-	public void eliminarFicha(FichaDomino fd) {
-		if(this.contieneFicha(fd)) {
-			juegoDomino.remove(fd);
-		}
-	}
-	
-	
+    private Set<FichaDomino> juegoDomino;
+
+    public Domino() {
+        super();
+        this.juegoDomino = new HashSet<>();
+
+        //llenamos el set
+        for (int i = 0; i <= 6; i++) {
+            for (int j = i; j <= 6; j++) {
+                juegoDomino.add(new FichaDomino(i, j));
+            }
+        }
+    }
+
+    public int numeroFichas() {
+        return juegoDomino.size();
+    }
+
+    public void introducirFicha(FichaDomino fd) {
+        juegoDomino.add(fd);
+    }
+
+    public boolean tieneFichas() {
+        return !juegoDomino.isEmpty();
+    }
+
+    public boolean contieneFicha(FichaDomino fd) {
+        //return juegoDomino.contains(fd);
+        Iterator<FichaDomino> it = juegoDomino.iterator();
+        while (it.hasNext()) {
+            if (it.next().equals(fd)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList fichas() {
+        ArrayList<FichaDomino> listaFichas = new ArrayList<>();
+        Iterator<FichaDomino> it = juegoDomino.iterator();
+        while (it.hasNext()) {
+            listaFichas.add((FichaDomino) it.next());
+        }
+        return listaFichas;
+    }
+
+    public void eliminarFicha(FichaDomino fd) {
+        if (this.contieneFicha(fd)) {
+            juegoDomino.remove(fd);
+        }
+    }
 
 }
